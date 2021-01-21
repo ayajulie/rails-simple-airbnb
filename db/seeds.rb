951a0e23164e91e451c2101b44f4b41f86e111c0
Aya Koffi <ayajulie@gmail.com>
2021-01-21 20:45:45 +0100

    Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+5.times do
+  Flat.create!(
+    name: 'A place to live',
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.review,
+    price_per_night: Faker::Number.between(from: 65, to: 150),
+    number_of_guests: Faker::Number.between(from: 2, to: 7)
+  )
+end
